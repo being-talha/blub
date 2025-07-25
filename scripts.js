6cfcec981617676b4548e913409841680543aa3e
Muhammad Talha <tlhmajid@gmail.com>
2025-07-25 20:29:41 +0500

    hi

--- scripts.js
+++ scripts.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 // Set theme based on system preference
 const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 if (!localStorage.getItem('theme')) {
   root.setAttribute('data-theme', systemTheme);
 }
-=======
-// Set theme based on system preference
-const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-if (!localStorage.getItem('theme')) {
-  root.setAttribute('data-theme', systemTheme);
-}
->>>>>>> a3f42fab53aaf35d41a1ccfce2fc4ab9f69aef0c
